bb8e5156c25ecc89fcc2a2555b93fa77e6165201
Alexander Garcia <alexandergv2117@gmail.com>
2024-03-20 23:47:18 -0500

    feat: create query for get user with role name

--- services/auth/src/repository/user.repository.go
+++ services/auth/src/repository/user.repository.go
@@ -2,13 +2,37 @@ package repository
 
 import (
 	"github.com/Alexandergv2117/store/src/config"
-	"github.com/Alexandergv2117/store/src/models"
 )
 
-func GetUserByEmail(email string) (models.User, error) {
-	var user models.User
+type UserRepository struct {
+	ID        string `gorm:"primaryKey;type:uuid;" json:"id"`
+	Username  string `gorm:"not null;unique;" json:"username"`
+	Password  string `gorm:"not null;" json:"password"`
+	Name      string `gorm:"not null;" json:"name"`
+	Lastname  string `gorm:"not null;" json:"lastname"`
+	Image     string `gorm:"not null;" json:"image"`
+	Email     string `gorm:"not null;unique;" json:"email"`
+	Phone     string `gorm:"not null;unique;" json:"phone"`
+	RoleID    string `gorm:"not null;" json:"role_id"` // Asegura que este campo corresponde a la FK
+	Role      string `gorm:"not null;" json:"role"`
+	CreatedAt string `gorm:"not null;" json:"created_at"`
+	UpdatedAt string `gorm:"not null;" json:"updated_at"`
+}
 
-	res := config.DB.Where("email = ?", email).First(&user)
+func GetUserByEmail(email string) (UserRepository, error) {
+	var user UserRepository
+
+	query := `
+		SELECT 
+			* 
+		FROM 
+			users 
+		LEFT JOIN 
+			roles ON users.role_id = roles.id 
+		WHERE email = ?
+		`
+
+	res := config.DB.Raw(query, email).Scan(&user)
 
 	return user, res.Error
 }
--- services/auth/src/services/auth.service.go
+++ services/auth/src/services/auth.service.go
@@ -1,16 +1,15 @@
 package services
 
 import (
-	"github.com/Alexandergv2117/store/src/models"
 	"github.com/Alexandergv2117/store/src/repository"
 )
 
-func Login(email string, password string) (models.User, error) {
+func Login(email string, password string) (repository.UserRepository, error) {
 
 	existUser, err := repository.GetUserByEmail(email)
 
 	if err != nil {
-		return models.User{}, err
+		return repository.UserRepository{}, err
 	}
 
 	return existUser, nil

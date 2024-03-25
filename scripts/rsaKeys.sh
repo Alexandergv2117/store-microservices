cfe675ab72b0608ccf96608dbd28056d8f4f90e8
Alexander Garcia <alexandergv2117@gmail.com>
2024-03-25 08:24:49 -0500

    fix: rename variables

--- scripts/rsaKeys.sh
+++ scripts/rsaKeys.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Establecer la ruta predeterminada
-ruta_archivos_default="../services/auth"
+default_dir="../services/auth"
 
 # Verificar si se proporciona una ruta como argumento
 if [ $# -eq 0 ]; then
     # Si no se proporciona ninguna ruta, utilizar la ruta predeterminada
-    ruta_archivos="$ruta_archivos_default"
+    dir_files="$default_dir"
 else
     # Si se proporciona una ruta como argumento, utilizar esa ruta
-    ruta_archivos="$1"
+    dir_files="$1"
 fi
 
 # Generar la clave privada
-openssl genrsa -out "$ruta_archivos/private.pem" 2048
+openssl genrsa -out "$dir_files/private.pem" 2048
 
 # Imprimir el contenido de la clave privada
 # echo "Contenido de private.pem:"
@@ -21,7 +21,7 @@ openssl genrsa -out "$ruta_archivos/private.pem" 2048
 # echo
 
 # Generar la clave pública a partir de la clave privada
-openssl rsa -pubout -in "$ruta_archivos/private.pem" -out "$ruta_archivos/public.pem"
+openssl rsa -pubout -in "$dir_files/private.pem" -out "$dir_files/public.pem"
 
 # # Imprimir el contenido de la clave pública
 # echo "Contenido de public.pem:"

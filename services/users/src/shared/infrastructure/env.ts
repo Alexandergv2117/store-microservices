208fbb27861255cbe19d3afc2f669ec56736b081
Alexander Garcia <alexandergv2117@gmail.com>
2024-06-12 21:19:59 -0500

    feat(users): create repository for upload image to s3

--- services/users/src/shared/infrastructure/env.ts
+++ services/users/src/shared/infrastructure/env.ts
@@ -6,3 +6,7 @@ export const DB_USERNAME = process.env.DB_USERNAME || 'postgres';
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'postgres';
 export const DB_NAME = process.env.DB_NAME || '';
 export const DB_LOGGING = process.env.DB_LOGGING === 'true';
+export const AWS_BUCKET_NAME_IMAGE = process.env.AWS_BUCKET_NAME_IMAGE || '';
+export const AWS_BUCKET_NAME_REGION = process.env.AWS_BUCKET_NAME_REGION || '';
+export const AWS_PUBLIC_KEY = process.env.AWS_PUBLIC_KEY || '';
+export const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY || '';
